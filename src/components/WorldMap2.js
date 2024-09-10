4893184a141c3f80d3ab1baed9cf168f812f1a9e
동헌희 <hui1601@naver.com>
2024-09-10 20:57:05 +0900

    clean code

--- src/components/WorldMap2.js
+++ src/components/WorldMap2.js
@@ -4,14 +4,14 @@ import { render } from '@testing-library/react'
 
 const WorldMap = () => {
     const mystyle = {
-        backgroundSize : "1000px 384px;" 
+        backgroundSize: "1000px 384px;"
     }
-    
+
     return (
         <div className='w-[1000px] h-auto'>
             <div className='w-[800px] h-auto m-auto'>
-                <div className='w-auto h-[336px] bg-[url("./img/worldmap2.jpg")] relative' style={{backgroundSize: "800px 336px" }}>
-                    <WorldMapPing/>
+                <div className='w-auto h-[336px] bg-[url("./img/worldmap2.jpg")] relative' style={{ backgroundSize: "800px 336px" }}>
+                    <WorldMapPing />
                 </div>
             </div>
         </div>
