afc8b02e73da211593a028a173ae62fa9d4469d9
동헌희 <hui1601@naver.com>
2024-09-11 10:45:29 +0900

    feat: map selection button

--- src/components/StatusChart.js
+++ src/components/StatusChart.js
@@ -5,11 +5,13 @@ import React, { useState, useEffect } from 'react'
 // 우측은 세로 2개로 분할해서 위에는 확인해야할 공격 개수, 아래는 미정
 
 const StatusChart = () => {
-    const [data, setData] = useState(0);
-
+    let [data, setData] = useState(0);
     useEffect(() => {
+        data = 0;
+        setData(data);
         const interval = setInterval(() => {
-            setData(Math.floor(Math.random() * 10000));
+            data = data + Math.floor(Math.random() * 100) / 10 - 2;
+            setData(data.toFixed(1));
         }, 1000);
 
         return () => clearInterval(interval);
@@ -19,17 +21,23 @@ const StatusChart = () => {
         <div className='w-full m-auto'>
             <div className='dataCard'>
                 <div className='grid grid-cols-2 grid-rows-2 gap-4 h-full w-full'>
-                    <div className='row-span-2 h-max'>
-                        <div className='text-2xl font-bold'>24시간 내 들어온 공격 개수</div>
-                        <div className='text-4xl font-bold'>{data}K</div>
+                    <div className='row-span-2 align-middle h-full'>
+                        <div>
+                            <div className='text-2xl font-bold'>Detected Threats(24h)</div>
+                            <div className='text-4xl font-bold'>{data}K</div>
+                        </div>
                     </div>
-                    <div>
-                        <div className='text-2xl font-bold'>우측에 2개 정도로 위 아래 나눠서 확인해야할 공격 개수</div>
-                        <div className='text-4xl font-bold'>미정</div>
+                    <div className='h-full'>
+                        <div>
+                            <div className='text-2xl font-bold'>Possible Attacks</div>
+                            <div className='text-4xl font-bold'>10</div>
+                        </div>
                     </div>
                     <div className='col-start-2 row-start-2 h-full'>
-                        <div className='text-2xl font-bold'>우측에 2개 정도로 위 아래 나눠서 확인해야할 공격 개수</div>
-                        <div className='text-4xl font-bold'>미정</div>
+                        <div>
+                            <div className='text-2xl font-bold'>Temporarily Blocked</div>
+                            <div className='text-4xl font-bold'>5</div>
+                        </div>
                     </div>
                 </div>
             </div>
